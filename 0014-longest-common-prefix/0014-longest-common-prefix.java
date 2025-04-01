076ffb89581faf545dd5d25fc34b151970031971
vennela343 <vennelamaisan@gmail.com>
2025-04-01 18:41:51 +0530

    Time: 1 ms (65.03%), Space: 41.9 MB (32.61%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int i = 0;
+        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+        return first.substring(0, i);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] words = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: " + solution.longestCommonPrefix(words));
+    }
+}
+   
+  
